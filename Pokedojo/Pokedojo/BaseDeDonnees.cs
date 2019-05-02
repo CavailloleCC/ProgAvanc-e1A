a8f9ecc8391ef5c1a5ec3c5f40dc12226d7c549d
CavailloleCC <48759097+CavailloleCC@users.noreply.github.com>
2019-05-02 14:10:41 +0200

    class BaseDeDonnees

---
+++ Pokedojo/Pokedojo/BaseDeDonnees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedojo
+{
+    class BaseDeDonnees
+    {
+        List<Pokemon> chiffres = new List<int>(); // création de la liste
+
+    }
+}
