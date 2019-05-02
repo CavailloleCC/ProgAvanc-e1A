17c8291a51ae32d6b4d2cdd9bb15d8d95c1bc01f
AlexaneGorbatoff <49560759+AlexaneGorbatoff@users.noreply.github.com>
2019-05-02 15:03:44 +0200

    classe Equipe

--- Pokedojo/Pokedojo/Equipe.cs
+++ Pokedojo/Pokedojo/Equipe.cs
@@ -8,5 +8,19 @@ namespace Pokedojo
 {
     class Equipe
     {
+        List<Pokemon> _listEquipe;
+        private Random _alea;
+        protected static int _numeroEquipe = 0;
+        public int Numero { get; protected set; }
+
+        public Equipe()
+        {
+            Numero = ++_numeroEquipe;
+            _listEquipe = new List<Pokemon>;
+            for(int i=0; i<3; i++)
+            {
+                _listEquipe.Add();
+            }
+        }
     }
 }
