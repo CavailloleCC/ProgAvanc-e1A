2f33aafdb54a57f7317538f91bb57207a91603b6
AlexaneGorbatoff <49560759+AlexaneGorbatoff@users.noreply.github.com>
2019-05-02 14:15:36 +0200

    classe Pokemon

---
+++ Pokedojo/Pokedojo/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedojo
+{
+    class Pokemon
+    {
+        private string _nom;
+        private int _pv;
+        private int _puissance;
+        private string _type;
+        private string _faiblesse;
+        private string _attaqueSpe;
+
+        public Pokemon(string nom, int pv, int puissance, string type, string faiblesse, string attaqueSpe)
+        {
+            _nom = nom;
+            _pv = pv;
+            _puissance = puissance;
+            _type = type;
+            _faiblesse = faiblesse;
+            _attaqueSpe = attaqueSpe;
+        }
+
+        public Pokemon(string nom, int pv, int puissance, string type, string faiblesse): this(nom, pv, puissance, type, faiblesse, "")
+        { }
+    }
+}
