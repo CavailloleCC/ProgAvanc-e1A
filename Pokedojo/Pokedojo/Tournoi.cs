5da1538a8743f97c0a56fec1d23e14a6cb1933e5
AlexaneGorbatoff <49560759+AlexaneGorbatoff@users.noreply.github.com>
2019-05-03 10:38:26 +0200

    Tournoi

--- Pokedojo/Pokedojo/Tournoi.cs
+++ Pokedojo/Pokedojo/Tournoi.cs
@@ -8,5 +8,22 @@ namespace Pokedojo
 {
     class Tournoi
     {
+        public List<Equipe> ListeTournoi { get; set; }
+        public BaseDeDonnees BddPokemon { get; set; }
+        private Random _alea;
+
+        public Tournoi()
+        {
+            BddPokemon = new BaseDeDonnees();
+            ListeTournoi = new List<Equipe>();
+            Equipe equipeTournoi;
+            for(int i=0;i<16;i++)
+            {
+                equipeTournoi = new Equipe(BddPokemon);
+                ListeTournoi.Add(equipeTournoi);
+            }
+        }
+
+
     }
 }
